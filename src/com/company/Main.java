f225f24f32dcc124a0093525eec65a4a89de7777
Elvin <elvin_abdullayev1@mail.ru>
2018-10-10 19:48:15 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,218 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int eded=scanner.nextInt();
+//        if(10000<=eded&&eded<100000) {
+//            int birinci = eded / 10000;
+//            int ikinci = (eded / 1000) % 10;
+//            int ucuncu = (eded / 100) % 10;
+//            int dorduncu = (eded / 10) % 10;
+//            int besinci = eded % 10;
+//            System.out.println(birinci+" "+ikinci+" "+ucuncu+" "+dorduncu+" "+besinci);
+//        }
+
+//        System.out.print("a-ni daxil edin");
+//        float a;
+//        a=scanner.nextInt();
+//        System.out.print("b-ni daxil edin");
+//        float b;
+//        b=scanner.nextInt();
+//        System.out.print("c-ni daxil edin");
+//        float c;
+//        c=scanner.nextInt();
+//        float d=b*b-4*a*c;
+//        float x1= (float) (-b + Math.sqrt(d) / (2 * a));
+//        float x2= (float) (-b - Math.sqrt(d) / (2 * a));
+//        System.out.println("x1="+x1);
+//        System.out.println("x2="+x2);
+
+
+//        System.out.print("birinci ededi daxil edin");
+//        int sum=0;
+//        int a;
+//        a=scanner.nextInt();
+//        System.out.print("ikinci ededi daxil edin");
+//        int b;
+//        b=scanner.nextInt();
+//        int orta=b-a+1;
+//        for(int i=a;i<=b;i++){
+//            sum=sum+i;}
+//            float edediort=sum/orta;
+//            System.out.println("ededlerin cemi="+sum);
+//            System.out.print("ededdi orta="+edediort);
+
+
+//        System.out.print("eded daxil edin");
+//        int N;
+//        N=scanner.nextInt();
+//        for(int i=1;i<N;i++){
+//           int kub=i*i*i;
+//            System.out.println(+kub);
+//        }
+
+//        System.out.print("eded daxil edin");
+//        int N;
+//        N=scanner.nextInt();
+//        for(int i=0;i<N;i=i+2){
+//            System.out.print(" "+i);
+//        }
+
+//     **   System.out.print("eded daxil edin");
+//        int n=scanner.nextInt();
+//        for(int i=1;i<n;i++){
+//           for(int j=1;j<n;j++){
+//                i=j*j;
+//               System.out.print(+i);
+//            }
+//            }
+
+
+//      **  System.out.print("eded daxil edin");
+//        int N;
+//        N=scanner.nextInt();
+//        for(int i=0;i<N;i=i*2){
+//            System.out.print(" "+i);
+//        }
+
+
+//        for(int i=1; i<10; i++) {
+//            for(int j=1; j<10; j++){
+//                System.out.print(i);
+//                System.out.print(" * ");
+//                System.out.print(j);
+//
+//                System.out.print(" = ");
+//
+//                System.out.println(i*j);
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1;i<=5;i++){
+//            System.out.print(i);
+//            System.out.print("\t");
+//            System.out.print(i*10);
+//            System.out.print("\t");
+//            System.out.print(i*100);
+//            System.out.println();
+//        }
+
+
+//        int largest=scanner.nextInt();
+//      for(int i=1;i<=10;i++){
+//      int current=scanner.nextInt();
+//      if(current>largest){
+//        largest=current;
+//     }
+//     }
+//      System.out.print(largest);
+
+
+//        for(int i=1;i<5;i++){
+//            for(int a=1;a<=i;a++){
+//
+//                System.out.print(" "+a);
+//            }
+//            System.out.println(" ");
+//        }
+
+
+//       * for(int i=1;i<5;i++){
+//            for(int a=1;a<=i;a=a+1){
+//                System.out.print(a);
+//            }
+//            System.out.print(i);
+//            System.out.println(" ");
+//        }
+
+//        int a=0;
+//        for(int i=1;i<5;i++){
+//            for(int j=1;j<=i;j++){
+//                a++;
+//                System.out.print(a);
+//            }
+//            System.out.println(" ");
+//        }
+
+
+
+//        for(int i=1;i<11;i++){
+//            for(int a=1;a<=i;a++){
+//                System.out.print("*");
+//
+//            }
+//            System.out.println(" ");
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
